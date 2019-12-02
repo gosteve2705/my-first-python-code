fa5217f755dbe5ac9169a91e2a8a02c43f630da8
Nomuoja Oghenemaro <38343496+gosteve2705@users.noreply.github.com>
2019-12-02 06:29:42 +0600

    Add files via upload

---
+++ largest number check.py
@@ -0,0 +1,10 @@
+a = int(input("enter a"))
+b = int(input("enter b"))
+c = int(input("enter c"))
+if a>b and a>c:
+             print("a is the largest")
+if b>a and b>c:
+             print("b is the largest")
+if c>a and c>b:
+             print("c is the largest")
+         
