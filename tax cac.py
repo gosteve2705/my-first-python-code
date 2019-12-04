dbaed1aedbc458a362748396a0a4f4978ba41ccb
Nomuoja Oghenemaro <38343496+gosteve2705@users.noreply.github.com>
2019-12-04 18:12:58 +0600

    Add files via upload

---
+++ tax cac.py
@@ -0,0 +1,21 @@
+
+state = str(input('enter your state  '))
+purchase_amount = int(input("enter the purchase amount  "))
+
+
+if state == 'CA':
+    tax_amount = .075
+    total_cost = purchase_amount*(1+tax_amount)
+    result = print("Since you're from {}, your total cost is {}.".format(state, total_cost))
+
+elif  state == 'MN':
+    tax_amount = .095
+    total_cost = purchase_amount*(1+tax_amount)
+    result = print("Since you're from {}, your total cost is {}.".format(state, total_cost))
+
+elif state == 'NY':
+    tax_amount = .089
+    total_cost = purchase_amount*(1+tax_amount)
+    result = print("Since you're from {}, your total cost is {}.".format(state, total_cost))
+
+
